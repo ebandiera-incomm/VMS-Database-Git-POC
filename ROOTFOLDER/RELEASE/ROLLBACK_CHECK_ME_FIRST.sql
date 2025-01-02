3b6973096ff32ae7458472974a36e7dac2242cbe
Evandro Bandiera <ebandiera@incomm.com>
2025-01-02 16:57:26 -0300

    Release r91 (#28)
    * standard release changes
    
    * changes procedures and fixed name for DEC to JAN
    
    * changed folder name
    
    * name folder
    
    * doc files changes
    
    * zip file added

---
+++ ROOTFOLDER/RELEASE/ROLLBACK_CHECK_ME_FIRST.sql
@@ -0,0 +1,48 @@
+spool $VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/LOG/ROLL_CHECK_ME_FIRST.log;
+
+SET SERVEROUTPUT ON;
+
+DECLARE 
+
+
+V_CIV_VERS_BULD VMSCMS.CMS_INCOMM_VERSION.CIV_VERS_BULD%TYPE;
+
+BEGIN
+
+        SELECT CIV_VERS_BULD
+        INTO   V_CIV_VERS_BULD
+        FROM   VMSCMS.CMS_INCOMM_VERSION 
+        WHERE CIV_BASE_VERS ='DB - 3.5.1';
+        
+                   
+           IF V_CIV_VERS_BULD = 'VMSGPRHOST_R91_B0002' 
+        THEN 
+		
+	    DBMS_OUTPUT.PUT_LINE('*************************************************');					
+            
+        DBMS_OUTPUT.PUT_LINE('PLEASE EXECUTE ROLLBACK FOR RELEASE VMSGPRHOST_R91_B0002');
+                         
+	    DBMS_OUTPUT.PUT_LINE('*************************************************');			
+						 
+        ELSIF V_CIV_VERS_BULD = 'VMSGPRHOST_R91_B0001' 
+        THEN
+
+	    DBMS_OUTPUT.PUT_LINE('***********************************************');					
+		
+        DBMS_OUTPUT.PUT_LINE('ROLLBACK IS ALREADY EXECUTED FOR VMSGPRHOST_R91_B0002');
+
+	    DBMS_OUTPUT.PUT_LINE('***********************************************');			        
+            
+        END IF;     
+        
+EXCEPTION WHEN NO_DATA_FOUND
+THEN
+     DBMS_OUTPUT.PUT_LINE('DATABASE RELEASE NOT FOUND IN VERSION MASTER');       
+                     
+END; 
+/
+
+spool off;
+
+
+
---
+++ ROOTFOLDER/RELEASE/ROLLBACK_VMSGPRHOST_RELEASE.sql
@@ -0,0 +1,66 @@
+SPOOL $VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/LOG/ROLLBACK_VMSCMS_RELEASE_R91.log;
+
+ 
+----------------------------------------------------------------------------------------------------------------
+--    Please keep JAN_VMSGPRHOST_R91_RELEASE folder in $VMS_HOME/ and then execute the script                        
+--    execute all the scripts using SQL prompt of Oracle only.                             
+--    Please take a proper back up of schema before executing this scripts                    
+------------------------------------------------------------------------------------------------------------------------------------            
+    --- RELEASE NAME : Release 
+    --- RELEASED FOR : Defect IDs : 
+    --- RELEASED BY  : DB Team  on 
+    --- PREREQUISITE FOR THIS BUILD : This script is to be executed on a environment where  is available
+------------------------------------------------------------------------------------------------------------------------------------        
+set define off;
+set lines 1010;
+set pages 1010; 
+set serveroutput on
+
+ 
+
+PROMPT               START OF RELEASE ROLLBACK_VMSGPRHOST_R91
+------------------------------------------------------------------------------------------------------------------------------------        
+SET ECHO OFF
+SET DEFINE OFF
+
+--ROLLBACK
+PROMPT *** Build 3 ***			
+	PROMPT *** DDL ROLLBACK ***	
+    PROMPT     *** PROCEDURES  *****
+    @$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/SP_CHW_ORDER_REPLACE.prc
+    @$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/SP_PREAUTH_AUTH_MERCTXN_ISO93.prc
+    @$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/SP_PREAUTH_TXN_ISO93.prc
+    @$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/SP_REQUISITION_ID.prc        
+    PROMPT     --------------------------------------------------------------------------------
+
+    PROMPT *** DML ROLLBACK ***
+
+PROMPT *** Build 2 ***			
+    PROMPT *** DDL ROLLBACK ***
+	--PROMPT     *** CMS_INST_PARAM.sql *****
+    --@$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/CMS_INST_PARAM.sql
+    PROMPT     --------------------------------------------------------------------------------
+
+    PROMPT *** DML ROLLBACK ***
+
+PROMPT *** Build 1 ***			
+	PROMPT *** DDL ROLLBACK ***
+	--PROMPT     *** SP_DISPUTE_PROCESS *****
+    --@$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/SP_DISPUTE_PROCESS.prc
+    PROMPT     --------------------------------------------------------------------------------
+	
+    PROMPT *** DML ROLLBACK ***
+    --PROMPT     *** CMS_RESPONSE_MAST *****
+    --@$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/CMS_RESPONSE_MASTB1.sql
+    PROMPT     --------------------------------------------------------------------------------
+
+    @$VMS_HOME/JAN_VMSGPRHOST_R91_RELEASE/ROOTFOLDER/RELEASE/ROLLBACK/MANDATORY_DML_ROLLBACK.sql;
+
+PROMPT			 	END OF RELEASE ROLLBACK_VMSGPRHOST_R91
+
+spool off;
+
+
+
+
+
